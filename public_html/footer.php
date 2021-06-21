9ce010f487dc22ed4e84e3494bad750bfb5c7c6b
Javier Anadón <javiand96@gmail.com>
2021-06-21 18:47:36 +0200

    Ejercicio 2

---
+++ public_html/footer.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+
+<footer class="flex-container">
+<p class="flex-item4">Datos de contacto:<br>Correo electronico: javiand96@gmail.com<br><i class="far fa-copyright"></i> 2021 Ningun derecho reservado ya que todo el contenido de la pagina es sacado de google imagenes</p>
+</footer>
+
---
+++ public_html/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<header class="flex-container">
+    <h1 class="flex-item6" style="background-image: url('images/banner.png'); background-repeat: no-repeat;  background-position: center;"></h1>
+    <p class="flex-item7" style="background-image: url('images/logojao.PNG'); background-repeat: no-repeat; background-size: 100% 100%; background-position: center;"></p>
+    <ul class="flex-container">
+        <li class="flex-item8-1" style="background-image: url('images/iconofb.png'); background-size: 100% 100%;"></li>
+        <li class="flex-item8-2" style="background-image: url('images/iconotw.png'); background-size: 100% 100%;"></li>
+        <li class="flex-item8-3" style="background-image: url('images/iconotwitch.png'); background-size: 100% 100%;"> </li>
+    </ul>
+</header>
